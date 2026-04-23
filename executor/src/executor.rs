288066f6c11d811dba9ced66de03840a5b58a639
yaukoo <yaukoo@126.com>
2026-04-23 21:17:48 +0800

    Executor中添加State对象，实现B指令状态保存功能

--- executor/src/executor.rs
+++ executor/src/executor.rs
@@ -1,32 +1,30 @@
 use crate::pose::Pose;
+use crate::state::State;
 
 pub struct Executor {
     pose: Pose,
+    /// 当前行驶状态，初始为正常状态
+    state: State,
 }
 
 impl Executor {
     pub fn with_pose(pose: Pose) -> Self {
-        Executor { pose }
+        Executor {
+            pose,
+            state: State::Normal,
+        }
     }
 
     pub fn execute(&mut self, cmds: &str) {
         for cmd in cmds.chars() {
             match cmd {
+                // B 指令：切换倒车状态
+                'B' => self.state.toggle_reverse(),
                 'M' => self.pose.forward(),
                 'L' => self.pose.turn_left(),
                 'R' => self.pose.turn_right(),
                 _ => (),
             }
-        }
-    }
-    /// 向当前朝向前进一格
-    fn forward(&mut self) {
-        match self.pose.heading {
-            'E' => self.pose.x += 1,
-            'S' => self.pose.y -= 1,
-            'W' => self.pose.x -= 1,
-            'N' => self.pose.y += 1,
-            _ => (),
         }
     }
 
